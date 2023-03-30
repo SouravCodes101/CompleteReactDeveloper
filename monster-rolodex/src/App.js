8da234f8b0e3859fbb544c48a9f36fa68aaf826b
SouravCodes101 <saurabc987@gmail.com>
2023-03-30 07:46:40 +0530

    Input Search Box

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -7,9 +7,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     }
+    console.log("constructor");
   }
 
   componentDidMount() {
+    console.log("componentDidMount");
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => { return response.json() })
       .then((users) => this.setState(() => {
@@ -21,8 +23,12 @@ class App extends Component {
   }
 
   render() {
+    console.log("render");
     return (
       <div className="App">
+        <input className={"search-box"} type="search" placeholder="search monsters" onChange={(event)=>{
+          console.log(event.target.value);
+        }}/>
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
