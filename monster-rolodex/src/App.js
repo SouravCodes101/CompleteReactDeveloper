c86c14ac716a4d9b060ee52ea09b96b9def0ce49
Sourav <sourav.choudhury@positra.com>
2023-03-30 16:03:35 +0530

    search

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -22,13 +22,26 @@ class App extends Component {
         }))
   }
 
+
+
   render() {
     console.log("render");
     return (
       <div className="App">
-        <input className={"search-box"} type="search" placeholder="search monsters" onChange={(event)=>{
-          console.log(event.target.value);
-        }}/>
+        <input className={"search-box"}
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchString = event.target.value.toLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            })
+          }} />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
